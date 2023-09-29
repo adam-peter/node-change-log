b00ea30e37d313cbf9a9b2555de90041e35a7e02
badmood111 <petrzela.adam@seznam.cz>
2023-09-29 06:08:01 +0200

    setup router and api routes

---
+++ src/router.ts
@@ -0,0 +1,46 @@
+import { Router } from "express";
+
+const router = Router();
+
+/**
+ * Product
+ */
+router.get("/product", () => {});
+
+//dynamic request parameter id
+router.get("/product/:id", () => {});
+
+//put = complete update of the record; patch = updates only updated properties
+router.put("/product/:id", () => {});
+
+router.post("/product", () => {});
+
+router.delete("/product/:id", () => {});
+
+/**
+ * Update
+ */
+router.get("/update", () => {});
+
+router.get("/update/:id", () => {});
+
+router.put("/update/:id", () => {});
+
+router.post("/update", () => {});
+
+router.delete("/update/:id", () => {});
+
+/**
+ * Update Points
+ */
+router.get("/updatepoint", () => {});
+
+router.get("/updatepoint/:id", () => {});
+
+router.put("/updatepoint/:id", () => {});
+
+router.post("/updatepoint", () => {});
+
+router.delete("/updatepoint/:id", () => {});
+
+export default router;
